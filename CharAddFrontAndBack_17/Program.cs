36b2f8648eda9b7779724a507999771958d73821
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-02 07:38:16 -0800

    2020_02_01

---
+++ CharAddFrontAndBack_17/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharAddFrontAndBack_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = Console.ReadLine();
+            Console.WriteLine(AddFandB(sentence));
+        }
+
+        static string AddFandB(string str)
+        {
+            return str.Substring(0, 1) + str + str.Substring(0, 1);
+        }
+    }
+}
