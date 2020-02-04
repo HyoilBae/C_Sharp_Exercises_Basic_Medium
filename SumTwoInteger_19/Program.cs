100894fc9b6bf0cd61768782dce19359a43272bd
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-04 10:03:33 -0800

    2020_02_04

---
+++ SumTwoInteger_19/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumTwoInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstInt = Convert.ToInt32(Console.ReadLine());
+            int secondInt = Convert.ToInt32(Console.ReadLine());
+
+            int result = twoSum(firstInt, secondInt);
+            Console.WriteLine(result);
+        }
+
+        public static int twoSum(int a, int b)
+        {
+            return a == b ? (a + b) * 3 : a + b;
+        }
+    }
+}
