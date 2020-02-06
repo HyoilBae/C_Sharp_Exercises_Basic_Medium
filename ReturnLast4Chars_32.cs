261fd5f71ebe1e04de9718506547982458286ccb
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-06 11:50:53 -0800

    Create ReturnLast4Chars_32.cs

---
+++ ReturnLast4Chars_32.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnLast4Chars_32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your punch line please");
+            //string testString = "The quick borwn fox jumps over the lazy dog.";
+            string testString = Console.ReadLine();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write(testString.Substring((testString.Length -4), 4));
+            }
+        }
+    }
+}
