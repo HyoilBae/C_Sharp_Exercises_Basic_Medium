ba8ef95dba71da76c22b83fbe23de610e40dc3c3
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-06 11:59:11 -0800

    Create StartWithString_34.cs

---
+++ StartWithString_34.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartWithString_34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "Hello how are you?";
+
+            //while (test.StartsWith("Hello"))
+            //{
+            //    Console.WriteLine(true);
+            //}
+
+            string str;
+            Console.Write("Input a string : ");
+            str = Console.ReadLine();
+            Console.WriteLine((str.Length < 6 && str.Equals("Hello")) || (str.StartsWith("Hello") && str[5] == ' '));
+        }
+    }
+}
