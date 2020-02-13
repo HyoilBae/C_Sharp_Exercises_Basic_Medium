16cbee8e5078463e12e2880f6fea9b78a86e30e1
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-13 09:01:36 -0800

    2020_02_13

---
+++ sumTwoIntWithOutPlusSign/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumTwoIntWithOutPlusSign
+{
+    class Solution
+    {
+        public int Getsum(int a, int b)
+        {
+            int[] intSum = new int[2];
+            intSum[0] = a;
+            intSum[1] = b;
+            int result = intSum.Sum();
+            return result;
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.Getsum(7, 2));
+        }
+    }
+}
