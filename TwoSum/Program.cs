b0c26c4e34d0c54b2cd6c1b74ccc7ebbdc063f01
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-13 09:01:21 -0800

    2020_02_13

---
+++ TwoSum/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Solution S = new Solution();
+            int[] array1 = { 2, 7, 11, 15 };
+            S.twoSum(array1, 9);
+            
+        }
+    }
+
+
+    public class Solution
+    {
+        public int[] twoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[j] == target - nums[i])
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            throw new IllegalArgumentException("No two sum solution");
+        }
+    }
+}
+
+
+    /*
+     * public int[] twoSum(int[] nums, int target) {
+    for (int i = 0; i < nums.length; i++) {
+        for (int j = i + 1; j < nums.length; j++) {
+            if (nums[j] == target - nums[i]) {
+                return new int[] { i, j };
+            }
+        }
+    }
+    throw new IllegalArgumentException("No two sum solution");
+}
+
+    */
+
