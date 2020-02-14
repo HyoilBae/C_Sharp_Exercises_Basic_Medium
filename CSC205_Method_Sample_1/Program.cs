5a999030f1f8d79abdf9710a3043980a7b80af09
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-14 08:42:33 -0800

    2020_02_14

---
+++ CSC205_Method_Sample_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC205_Creating_Methods
+{
+    class Program
+    {
+
+        /*
+         * 
+         * Reference: https://www.w3resource.com/csharp-exercises/function/csharp-function-exercise-1.php
+         * This function will pop up greeting messages 
+         * 
+         * 
+         */
+        static void Main(string[] args)
+        {
+            //calling welcome method
+            welcome();
+            //calling HaveAnice method
+            HaveAnice();
+        }
+
+        //This methods will print out Welcome Friends!
+        public static void welcome()
+        {
+            Console.WriteLine("Welcome Friends!");
+        }
+
+        //This methods will print out welcome Friends!
+        public static void HaveAnice()
+        {
+            Console.WriteLine("Have a nice day!");
+        }
+
+    }
+}
