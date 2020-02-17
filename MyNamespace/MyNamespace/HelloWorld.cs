efe5432f7a25751ff9bf396fc9b6911dfb036647
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-17 11:42:42 -0800

    2020_02_17

---
+++ MyNamespace/MyNamespace/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
