948bc2b78bf2667fdd9cff2e58b0291b93f0a573
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-17 11:45:04 -0800

    2020_02_17

---
+++ ConsolePjt/ConsolePjt/ConsoleEx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsolePjt
+{
+    class ConsoleEx
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello");
+            Console.Write(" ");
+            Console.WriteLine("World!");
+
+            Console.WriteLine(Console.ReadLine());
+
+            Console.WriteLine("Hello " + args[0]);
+            
+        }
+    }
+}
