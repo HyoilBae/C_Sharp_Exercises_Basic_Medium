91cd677aa9e5915b37db54f97469199719628d6d
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-24 11:01:00 -0800

    Create _47SumOfArray.cs

---
+++ _47SumOfArray.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _47SumOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] Array1 = new int[10];
+
+            int i = 0;
+            while (i < 10)
+            {
+
+                Array1[i] = (random.Next(1, 10));
+                i++;
+            }
+
+
+
+            Console.WriteLine(string.Join(", ", Array1));
+
+
+
+            sumArray(Array1);
+
+        }
+
+        public static void sumArray(int[] arr)
+        {
+            int sum = 0;
+            foreach (var item in arr)
+            {
+                sum = sum + item;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
