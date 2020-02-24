10c84e57d2568d81e570fa15a5b28d8d1a2f5103
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-24 14:48:30 -0800

    Create _52_HighestValueInArray.cs

---
+++ _52_HighestValueInArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _52_HightestValueInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] intArray = new int[20];
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                intArray[i] = random.Next(1, 20);
+            }
+
+            Console.WriteLine(string.Join(", ", intArray));
+
+            int biggestNumber = intArray[0];
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                if (biggestNumber < intArray[i])
+                {
+                    biggestNumber = intArray[i];
+                }
+            }
+
+            Console.WriteLine(biggestNumber);
+        }
+    }
+}
+ 
