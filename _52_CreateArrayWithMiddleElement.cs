500e3174a84cbd9899d2df3364086cd66a63a95b
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-25 11:01:01 -0800

    Update _52_CreateArrayWithMiddleElement.cs

--- _52_CreateArrayWithMiddleElement.cs
+++ _52_CreateArrayWithMiddleElement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace _52_CreateArrayWithMiddleElement
 {
@@ -10,7 +11,12 @@ namespace _52_CreateArrayWithMiddleElement
     {
         static void Main(string[] args)
         {
+            int[] array1 = { 1, 2, 5 };
+            int[] array2 = { 0, 3, 8 };
+            int[] array3 = { -1, 0, 2 };
 
+            //easy way
+            
         }
     }
 }
