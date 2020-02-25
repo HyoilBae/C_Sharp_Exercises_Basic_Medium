480430aa1df27d2dc2cfa51c2dac9eab2b1c6409
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-25 11:05:27 -0800

    Update _52_CreateArrayWithMiddleElement.cs

--- _52_CreateArrayWithMiddleElement.cs
+++ _52_CreateArrayWithMiddleElement.cs
@@ -16,7 +16,15 @@ namespace _52_CreateArrayWithMiddleElement
             int[] array3 = { -1, 0, 2 };
 
             //easy way
-            
+            int[] middleArray = new int[3];
+            middleArray[0] = array1[1];
+            middleArray[1] = array2[1];
+            middleArray[2] = array3[1];
+
+            Console.WriteLine(string.Join(", ", middleArray));
+
+            //what if 
+           
         }
     }
 }
