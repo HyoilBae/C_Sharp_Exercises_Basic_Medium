bf28348f31277ae4608e9fef8fb07eb5d80dbb22
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-25 10:40:51 -0800

    Update _48_FirstLastEqual.cs

--- _48_FirstLastEqual.cs
+++ _48_FirstLastEqual.cs
@@ -10,6 +10,7 @@ namespace _48_FirstLastEqual
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Hello");
             int[] nums = { 1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1 };
             Console.WriteLine((nums.Length >= 1) && (nums[0].Equals(nums[nums.Length - 1])));
         }
