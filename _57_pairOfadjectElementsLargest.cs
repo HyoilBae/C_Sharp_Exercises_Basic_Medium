be8e00aa3598a2f8847aa6bba6a0728eb0b5bdba
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-26 09:43:29 -0800

    Create _57_pairOfadjectElementsLargest.cs

---
+++ _57_pairOfadjectElementsLargest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _57_pairOfadjacentElementsHighest
+{
+    class Program
+    {
+        public static int adjacent_Elements_Product(int[] input_Array)
+        {
+            int max = input_Array[0] * input_Array[1];
+            for (int x = 1; x <= input_Array.Length - 2; x++)
+            {
+                max = Math.Max(max, input_Array[x] * input_Array[x + 1]);
+            }
+            return max;
+        }
+        public static void Main()
+        {
+            Console.WriteLine(adjacent_Elements_Product(new int[] { 1, -3, 4, -5, 1 }));
+            Console.WriteLine(adjacent_Elements_Product(new int[] { 1, 3, 4, 5, 2 }));
+            Console.WriteLine(adjacent_Elements_Product(new int[] { 1, 3, -4, 5, 2 }));
+            Console.WriteLine(adjacent_Elements_Product(new int[] { 1, 0, -4, 0, 2 }));
+        }
+    }
+}
