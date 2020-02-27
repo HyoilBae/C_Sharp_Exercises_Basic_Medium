369c30eb71f12fadeaeb6235e9ca117c1c2e91fc
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-26 16:51:10 -0800

    Update _Inheritance.cs

--- _Inheritance.cs
+++ _Inheritance.cs
@@ -13,8 +13,29 @@ namespace Inheritance
             User alice = new User("Alice", "Password");
             alice.Hello();
 
-            Console.WriteLine(alice.ToString());
-            Console.WriteLine(alice._PassWord);
+            //Console.WriteLine(alice.ToString());
+            //Console.WriteLine(alice._PassWord);
+
+            Student bob = new Student("bob", "Password");
+            bob.Hello();
+
+            User test1 = new Student();
+            test1.Hello();
+
+            User st1 = new User();
+            st1._UserName = "st1";
+            User st2 = new User();
+            st2._UserName = "st2";
+            User st3 = new User();
+            st3._UserName = "st3";
+
+            MagicList<User> MagicListUser = new MagicList<User>();
+            MagicListUser.Add(st1);
+            MagicListUser.Add(st2);
+            MagicListUser.Add(st3);
+            Console.WriteLine(MagicListUser.ChooseRandom());
+          
+
         }
     }
 }
