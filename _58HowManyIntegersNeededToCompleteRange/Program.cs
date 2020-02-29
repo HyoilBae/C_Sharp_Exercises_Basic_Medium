07504464b96d8f6733aee3ef819d698ef9aac972
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-28 16:36:01 -0800

    _58HowManyIntegersNeededToCompleteRange

---
+++ _58HowManyIntegersNeededToCompleteRange/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _58HowManyIntegersNeededToCompleteRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int[] intArr = new int[] { random.Next(1, 10) };
+
+            int intCount = intArr.Length;
+            Console.WriteLine(intCount);
+
+
+        }
+    }
+}
