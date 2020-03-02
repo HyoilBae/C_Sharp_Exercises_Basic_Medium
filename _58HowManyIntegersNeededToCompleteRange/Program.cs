8ea849ff12b01e6176b87a397c7f9463648163e0
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-02 08:55:44 -0800

    2020_03_02

--- _58HowManyIntegersNeededToCompleteRange/Program.cs
+++ _58HowManyIntegersNeededToCompleteRange/Program.cs
@@ -8,12 +8,35 @@ namespace _58HowManyIntegersNeededToCompleteRange
 {
     class Program
     {
-        static void intCount(int[] arr)
+        static void intCountArr(int[] arr)
         {
+            int count = 0;
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == numbers[i])
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
 
+        public static int consecutive_array(int[] input_Array)
+        {
+            Array.Sort(input_Array);
+            int ctr = 0;
+            for (int i = 0; i < input_Array.Length - 1; i++)
+            {
+                ctr += input_Array[i + 1] - input_Array[i] - 1;
+            }
+            return ctr;
         }
         static void Main(string[] args)
         {
+
+            Console.WriteLine(consecutive_array(new int[] { 1, 3, 5, 6, 9 }));
+            Console.WriteLine(consecutive_array(new int[] { 0, 10 }));
             //Random random = new Random();
             //int[] numbers = new int[10]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
@@ -25,13 +48,16 @@ namespace _58HowManyIntegersNeededToCompleteRange
 
             //}
 
-            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            int[] intArr = { 1, 3, 4, 7, 9 };
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //int[] intArr = { 1, 3, 4, 7, 9 };
 
-            int intCount = intArr.Length;
-            Console.WriteLine(intCount);
+            //int intCount = intArr.Length;
+            //Console.WriteLine(intCount);
 
-            Console.Write(string.Join(", ", intArr));
+            //Console.Write(string.Join(", ", intArr));
+
+            //intCountArr(intArr);
+            //Console.WriteLine();
 
 
         }
