83c7b6eb988faa74eed0b4674f58449fafee567b
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-02 17:37:02 -0800

    _61sortingIntegersWithoutMoving-5

---
+++ _61SortingIntegersWithoutMoving-5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _61SortingIntegersWithoutMoving_5
+{
+    class Program
+    {
+        public static int[] sort_numbers(int[] arra)
+        {
+
+            int[] num = arra.Where(x => x != -5).OrderBy(x => x).ToArray();
+            int ctr = 0;
+
+            return arra.Select(x => x >= 0 ? num[ctr++] : -5).ToArray();
+        }
+        static void Main(string[] args)
+        {
+            int[] x = sort_numbers(new int[] { -5, 236, 120, 70, -5, -5, 698, 280 });
+            foreach (var item in x)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
