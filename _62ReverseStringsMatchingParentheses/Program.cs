6220915e3f7ed2a7e093378cc26f699a7c9b59a9
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-02 17:41:23 -0800

    _62ReverseStringMatchingParentheses

---
+++ _62ReverseStringsMatchingParentheses/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _62ReverseStringsMatchingParentheses
+{
+    class Program
+    {
+        public static string reverse_remove_parentheses(string str)
+        {
+            int lid = str.LastIndexOf('(');
+            if (lid == -1)
+            {
+                return str;
+            }
+            else
+            {
+                int rid = str.IndexOf(')', lid);
+
+                return reverse_remove_parentheses(
+                      str.Substring(0, lid)
+                    + new string(str.Substring(lid + 1, rid - lid - 1).Reverse().ToArray())
+                    + str.Substring(rid + 1)
+                );
+            }
+        }
+
+            static void Main(string[] args)
+        {
+            Console.WriteLine(reverse_remove_parentheses("p(rq)st"));
+            Console.WriteLine(reverse_remove_parentheses("(p(rq)st)"));
+            Console.WriteLine(reverse_remove_parentheses("ab(cd(ef)gh)ij"));
+        }
+    }
+}
