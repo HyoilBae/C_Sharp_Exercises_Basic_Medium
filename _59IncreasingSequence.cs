ee97e4d49e57446fbc8f2a962a6a935b0891fdc1
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-02 17:16:18 -0800

    2020_03_02

---
+++ _59IncreasingSequence.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _59PossibleToCreateIncreasingSequence
+{
+    class Program
+    {
+        public static bool test_Increasing_Sequence(int[] int_seq)
+        {
+            int x = 0;
+            for (int i = 0; i < int_seq.Length - 1; i++)
+            {
+                if (int_seq[i] >= int_seq[i + 1])
+                    x++;
+                if (i + 2 < int_seq.Length && int_seq[i] >= int_seq[i + 2])
+                    x++;
+            }
+            return x <= 2;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test_Increasing_Sequence(new int[] { 1, 3, 5, 6, 9 }));
+            Console.WriteLine(test_Increasing_Sequence(new int[] { 0, 10 }));
+            Console.WriteLine(test_Increasing_Sequence(new int[] { 1, 3, 1, 3 }));
+        }
+    }
+}
