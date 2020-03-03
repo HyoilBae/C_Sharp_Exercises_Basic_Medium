0755248ee00c067a7fb20aa82ae8fee0e07d7af3
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-03 07:44:17 -0800

    Create _1_TwoSum_Same_Triple.cs

---
+++ _1_TwoSum_Same_Triple.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_TwoSum_Same_Triple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first number :");
+            int firstNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your second number :");
+            int secondNum = int.Parse(Console.ReadLine());
+
+            if (firstNum == secondNum)
+            {
+                Console.WriteLine((firstNum + secondNum) * 3);
+            }
+            else
+            {
+                Console.WriteLine((firstNum + secondNum));
+            }
+
+            int result = test(firstNum, secondNum);
+            Console.WriteLine(result);
+        }
+        public static int test(int x, int y)
+        {
+            return x == y ? (x + y) * 3 : x + y;
+        }
+    }
+}
