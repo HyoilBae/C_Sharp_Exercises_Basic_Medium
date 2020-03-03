50b08fd659fdfb6bf0d720fae7468e7b731b0be2
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-03 08:01:16 -0800

    Create _2_Absolute_Difference_Between_51.cs

---
+++ _2_Absolute_Difference_Between_51.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Absolute_Difference_Between_51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter you number :");
+            int userInt = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(absFromFiftyOne(userInt));
+        }
+
+        static int absFromFiftyOne(int n)
+        {
+            //int result;
+            //if (n > 51)
+            //{
+            //    result = (Math.Abs(51 - n)) * 3;
+            //    return result;
+            //}
+            //else
+            //{
+            //    result = (Math.Abs(51 - n));
+            //    return result;
+            //}
+
+            return n > 51 ? (Math.Abs(51 - n)) * 3 : (Math.Abs(51 - n));
+        }
+    }
+}
