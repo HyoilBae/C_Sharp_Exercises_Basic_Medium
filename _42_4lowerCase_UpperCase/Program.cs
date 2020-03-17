b1b416013b68d0193dca2907f24248c41588f363
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-17 08:32:23 -0700

    2020_03_17

---
+++ _42_4lowerCase_UpperCase/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _42_4lowerCase_UpperCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your string : ");
+            string userInput = Console.ReadLine();
+            caseChanger(userInput);
+        }
+
+        static void caseChanger(string str)
+        {
+            if (str.Length < 4)
+            {
+                Console.WriteLine(str.ToUpper());
+            }
+            else if (str.Length >= 4)
+            {
+                string first4 = str.Substring(0, 4);
+              
+                Console.WriteLine(first4.ToLower() + str.Substring(4, str.Length - 4));
+            }
+            else
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+}
