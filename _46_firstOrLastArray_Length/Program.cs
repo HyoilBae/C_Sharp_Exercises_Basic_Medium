b7984e89256bbfadcab3b66b1b0b7d80afa1af3d
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-17 08:34:59 -0700

    2020_03_17

---
+++ _46_firstOrLastArray_Length/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _46_firstOrLastArray_Length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine("Enter a number : ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            List<int> intList = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                intList.Add(random.Next(1, 10));
+            }
+
+            foreach (var item in intList)
+            {
+                Console.WriteLine(item + " ");
+            }
+
+            bool result = firstLast(intList, userInput);
+            Console.WriteLine($"result is {result} and the list has {intList.Count} long");
+        }
+
+        static bool firstLast(List<int> list, int n)
+        {
+
+           
+
+                if (list[0] == n || list[list.Count -1] == n)
+                {
+                    return true;
+                   
+                }
+                else
+                {
+                    return false;
+                    
+                }
+                      
+            
+            
+        }
+
+        
+    }
+}
