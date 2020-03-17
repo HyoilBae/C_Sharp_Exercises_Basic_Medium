e8607c6eda83d19cdd2a9b75c31d397691502f03
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-17 08:32:55 -0700

    2020_03_17

---
+++ _43_immediatelyFollowedByTwoWW/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _43_immediatelyFollowedByTwoWW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter String : ");
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine(wCheck(userInput));
+        }
+        static bool wCheck(string str)
+        {
+            string firstTwo = str.Substring(0, 2);
+            string theThird = str.Substring(2, 1);
+            if (firstTwo == "ww" && theThird == "w")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public static bool test(string str)
+        {
+            var ctr = 0;
+            for (var i = 0; i < str.Length - 1; i++)
+            {
+                if (str[i].Equals('w')) ctr++;
+                if (str.Substring(i, 2).Equals("ww") && ctr > 2)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
