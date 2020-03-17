4c43294cdcdb4aadeb2e9bd0ed3de26b03a40fb7
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-17 08:33:18 -0700

    2020_03_17

---
+++ _44_oddPositionChracters/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace _44_oddPositionChracters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What? : ");
+            string userInput = Console.ReadLine();
+            //oddPosition(userInput);
+            //oddPositionArrList(userInput);
+            oddPositionList<string>(userInput);
+        }
+
+        static void oddPosition(string str)
+        {
+            int arrSize = str.Length;
+            string[] strArr = new string[arrSize / 2];
+            int iCount = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+               
+                if (i % 2 == 0)
+                {
+                strArr[iCount] = str[i].ToString();
+                    iCount++;
+                }
+            }
+
+            foreach (var item in strArr)
+            {
+                Console.Write(item);
+            }
+        }
+
+        static void oddPositionArrList(string str)
+        {
+            ArrayList strArr = new ArrayList();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i%2 == 0)
+                {
+                    strArr.Add(str[i]);
+                }
+            }
+
+            foreach (var item in strArr)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+        }
+
+        static void oddPositionList<T>(string str)
+        {
+            Console.WriteLine(str);
+            List<char> strList = new List<char>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    strList.Add(str[i]);
+                }
+            }
+            foreach (var item in strList)
+            {
+                Console.Write(item +" ");
+            }
+        }
+    }
+}
