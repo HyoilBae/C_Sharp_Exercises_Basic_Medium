c18528ab28a8a19272147293542c3b4b3e2efe02
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-17 08:32:05 -0700

    2020_03_17

---
+++ _41WcharacterBetween1and3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _41WcharacterBetween1and3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter String : ");
+            string userInput = Console.ReadLine();
+            //.WriteLine(userInput.Substring(1, 1));
+            bool result = checkW(userInput);
+            Console.WriteLine(result);
+        }
+        static bool checkW(string str)
+        {
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ((str.Substring(i, 1) == "w") || (str.Substring(i, 1) == "W"))
+                {
+                    count += 1;
+                }
+                
+            }
+            if (count >= 1 && count <= 3)
+            {
+                //Console.WriteLine(true);
+                return true;
+            }
+            else
+            {
+                //Console.WriteLine(false);
+                return false;
+            }
+
+        }
+    }
+}
