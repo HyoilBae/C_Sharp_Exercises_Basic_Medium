2605fe688d76da0dd3c6fcba746d54b5000bf0ff
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-19 08:20:18 -0700

    _48_FirstLastEqual

---
+++ _48_FirstLastEqual/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _48_FirstLastEqual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1 };
+            Console.WriteLine((nums.Length >= 1) && (nums[0].Equals(nums[nums.Length - 1])));
+        }
+    }
+}
