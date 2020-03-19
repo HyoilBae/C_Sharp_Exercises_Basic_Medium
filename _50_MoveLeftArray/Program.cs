35e36afc2cce3b26fd53ecb81206816e4cf811c2
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-19 08:22:07 -0700

    _50_MoveLeftArray

---
+++ _50_MoveLeftArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _50_MoveLeftArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array1 = { 1, 2, 8 };
+            Console.WriteLine(string.Join(",", Array1));
+
+            var empty = 0;
+            empty = Array1[0];
+            Array1[0] = Array1[1];
+            Array1[1] = Array1[2];
+            Array1[2] = empty;
+
+            Console.WriteLine(string.Join(",", Array1));
+
+
+            Console.WriteLine("\nArray1: [{0}]", string.Join(", ", nums));
+            var temp = nums[0];
+            for (var i = 0; i < nums.Length - 1; i++)
+            {
+                nums[i] = nums[i + 1];
+            }
+            nums[nums.Length - 1] = temp;
+            Console.WriteLine("\nAfter rotating array becomes: [{0}]", string.Join(", ", nums));
+
+        }
+    }
+}
