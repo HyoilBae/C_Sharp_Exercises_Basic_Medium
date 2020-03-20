4101cf1ec118ae9bfd10a7afc8578a3e0e9deabf
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-20 13:38:06 -0700

    CheckOddNumber

---
+++ _53_CheckOddNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _53_CheckOddNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArr = { 2, 4, 7, 8, 6 };
+
+            for (int i = 0; i < intArr.Length; i++)
+            {
+                if (intArr[i] % 2 != 0)
+                {
+                    Console.WriteLine(true);
+                }
+                else
+                {
+                    Console.WriteLine(false);
+                }
+            }
+        }
+    }
+}
