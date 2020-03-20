be3b17120f8b32395657122e53c3ae6e64d3dd8e
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-20 13:38:36 -0700

    LargestAdjacentElements

---
+++ _55_LargestAdjacentElements/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _55_LargestAdjacentElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            GenericList genericList = new GenericList();
+            List<int> intList = new List<int>();
+
+            for (int i = 0; i < 20; i++)
+            {
+            intList.Add(random.Next(1, 20));
+            }
+            Console.Write(string.Join(", ", intList));
+            
+
+            //genericList.caclLargestAdjs<int>();
+           
+        }
+
+    }
+
+    class GenericList
+    {
+        public GenericList()
+        {
+            Console.WriteLine("Default Constructor");
+        }
+
+        public void caclLargestAdjs<T>(T para)
+        {
+
+        }
+    }
+}
+
+
