9964ab904a39f5027ff808f436c0f48810d35240
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-20 13:43:34 -0700

    Basic_20

---
+++ Basic_20/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_20
+{
+    class difference
+    {
+        public int twoDiff (int a, int b)
+        {
+            return a > b ? (Math.Abs(a - b)) * 2 : b - a;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstN = int.Parse(Console.ReadLine());
+            int secondN = int.Parse(Console.ReadLine());
+
+            difference d = new difference();
+            int result = d.twoDiff(firstN, secondN);
+            Console.WriteLine(result);
+        }
+    }
+}
