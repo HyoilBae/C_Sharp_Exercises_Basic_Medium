9378e58e1f21ca06dcaf8dfba502c9e21bdd5abf
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-25 06:54:34 -0700

    CountingSpacesInString

---
+++ CountingSpacesInString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingSpacesInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine();
+
+            spaceCounting(userInput);
+           
+        }
+        
+
+        static void spaceCounting(string str)
+        {
+            string spaceStr = " ";
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                string ifSpace = str.Substring(i, 1);
+                if (ifSpace == spaceStr)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
