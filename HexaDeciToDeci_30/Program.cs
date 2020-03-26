2a3d1a3ca64607c083222011fe6ef82cb148ddab
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-26 08:23:17 -0700

    HexaDeciToDeci

---
+++ HexaDeciToDeci_30/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexaDeciToDeci_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hexaDeci = "4B0";
+            Console.WriteLine("Hexadecimal number: " + hexaDeci);
+
+            int deci = int.Parse(hexaDeci, System.Globalization.NumberStyles.HexNumber);
+
+            Console.WriteLine("Convert to Decimal number: " + deci);
+        }
+    }
+}
