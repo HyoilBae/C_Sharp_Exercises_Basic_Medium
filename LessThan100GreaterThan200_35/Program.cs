94fccbb8c12e9ab37af08c771f8e2e1e8e814b11
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-26 08:33:02 -0700

    LessThan100GreaterThan200

---
+++ LessThan100GreaterThan200_35/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessThan100GreaterThan200_35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number : ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number : ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            if (firstNumber < 100 && secondNumber > 200)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
