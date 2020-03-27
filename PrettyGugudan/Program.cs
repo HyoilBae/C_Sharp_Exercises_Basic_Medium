04723a810c6b9cabbfd9aa32dd2439f79f569606
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-27 08:41:58 -0700

    PrettyGunudan

---
+++ PrettyGugudan/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrettyGugudan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 2; j < 10; j++)
+                {
+                    Console.Write($"{j} * {i} = {i * j}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
