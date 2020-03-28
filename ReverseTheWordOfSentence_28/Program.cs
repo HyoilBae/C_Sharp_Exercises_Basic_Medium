806ed6031753dbe1763d27dad35133189907f5fc
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-28 14:54:14 -0700

    ReverseTheWordOfSentence

---
+++ ReverseTheWordOfSentence_28/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ReverseTheWordOfSentence_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string randLine = Console.ReadLine();
+            Console.WriteLine("\n");
+            Console.WriteLine("Original String: {0}", randLine);
+            Console.WriteLine("\n");
+
+
+            string[] words = randLine.Split();
+            //string[] words = randLine.Split(new[] {" "}, StringSplitOptions.None);
+            Console.Write("string[] words = ");
+            foreach (string word in words)
+            {
+                Console.Write("[" + word + "]" + ", ");
+            }
+            Console.WriteLine("\n");
+
+
+            //List<string> wordsList = new List<string>();
+            ArrayList wordsList = new ArrayList();
+
+            string result = "";
+
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                result = result + words[i] + " ";
+            }
+            wordsList.Add(result);
+
+
+            foreach (String s in wordsList)
+            {
+                Console.WriteLine("Reverse String: " + s);
+            }
+            Console.WriteLine("\n");
+        }
+    }
+    
+}
