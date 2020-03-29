200ff4bf4a5c12156b2c146a28b46945f2771ae6
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-29 12:58:48 -0700

    Quiz1

---
+++ CSC440 Quiz 1/CSC440 Quiz 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC440_Quiz_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+
+            int b = 4;
+
+            int c = guess(ref a, b);
+
+            Console.WriteLine("{0}, {1}, {2}", a, b, c);
+
+        }
+
+        public static int guess(ref int first, int second)
+
+        {
+
+            int temp = first;
+
+            first = second;
+
+            second = temp;
+
+            return temp;
+
+        }
+    }
+}
