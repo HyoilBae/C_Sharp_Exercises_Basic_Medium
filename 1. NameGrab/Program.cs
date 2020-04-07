38a41ec5dc3e242901d0e9a7be4ce02d92391b0c
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-04-07 10:50:09 -0700

    NameGrab

---
+++ 1. NameGrab/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.NameGrab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // User Name
+            string name = "Farhad Hesam Abbasi";
+
+            // Get first letter
+            int charPosition = name.IndexOf("F");  //Locate the first Letter of his first name
+            Console.WriteLine(charPosition);
+            char firstLetter = name[charPosition];
+            Console.WriteLine(firstLetter);
+
+            // Get last name
+            charPosition = name.IndexOf("Abbasi");
+            Console.WriteLine(charPosition);
+            string lastName = name.Substring(charPosition);   //show the string of name from charPosition
+            Console.WriteLine(lastName);
+
+            // Print results
+            Console.WriteLine($"{firstLetter}. {lastName}");
+
+
+
+
+            string msg = "Yabba dabba doo!";
+
+            charPosition = msg.IndexOf("Y");
+            char firstChar = msg[charPosition];
+            Console.WriteLine(firstChar);
+        }
+    }
+}
---
+++ 1. NameGrab/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("1. NameGrab")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("1. NameGrab")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("71db3d91-39f9-4fde-99ff-84abb5a28c85")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
