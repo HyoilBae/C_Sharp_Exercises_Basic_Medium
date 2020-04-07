3d453ea268fd665e02fb339adc20b88c04f72e2c
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-04-07 10:51:34 -0700

    ConsoleCreatures

---
+++ 1.ConsoleCreatures/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.ConsoleCreatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" .-. ");
+            Console.WriteLine("(o o)");
+            Console.WriteLine("| o |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+            Console.WriteLine("Spooky Ghost");
+
+            Console.WriteLine("-Dancing Man-");
+            Console.WriteLine("c_     __");
+            Console.Write("  \\");
+            Console.WriteLine("__|__|__");
+
+        }
+    }
+}
