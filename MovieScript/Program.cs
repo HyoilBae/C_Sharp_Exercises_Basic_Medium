4416c70f9120ed4f1ae5f6fd1920b88bb52f32fa
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-04-10 11:52:58 -0700

    MovieScript

---
+++ MovieScript/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieScript
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Script line
+            string script = "Close on a portrait of the HANDSOME PRINCE -- as the BEAST'S giant paw slashes it.";
+
+            // Get camera directions
+            int charPosition = script.IndexOf("Close");
+            int length = "Close on".Length;
+            string cameraDirections = script.Substring(charPosition, length);
+
+            // Get scene description
+            charPosition = script.IndexOf("a portrait");
+            string sceneDescription = script.Substring(charPosition);
+
+            // Make camera directions uppercase
+            cameraDirections = cameraDirections.ToUpper();
+
+            // Make scene description lowercase
+            sceneDescription = sceneDescription.ToLower();
+
+            // Print results
+            Console.WriteLine($"{cameraDirections} {sceneDescription}");
+
+
+
+
+            //int num = 1;
+            //string population = "all";
+            //Console.WriteLine(num + " ring to rule them " + population);
+        }
+    }
+}
