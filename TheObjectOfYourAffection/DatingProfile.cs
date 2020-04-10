9584312acdeef1e23a1190bca15728987407ef9a
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-04-10 11:55:44 -0700

    TheObjectOfYourAffection

---
+++ TheObjectOfYourAffection/DatingProfile.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheObjectOfYourAffection
+{
+    class Profile
+    {
+        private string name;
+        private int age;
+        //private int Age
+        //{
+        //    get { return age; }
+        //    set
+        //    {
+        //        if (age >= 18)
+        //        {
+        //            age = value;
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("You must be over 18 yrs old to join us.");
+        //        }
+        //    }
+        //}
+
+        private string city;
+        private string country;
+        private string pronouns;
+        private string[] hobbies;
+
+        public Profile(string name = "n/a", int age = 0, string city = "n/a", string country = "n/a", string pronouns = "they/them")
+        {
+            this.name = name;
+            this.age = age;
+
+            this.city = city;
+            this.country = country;
+            this.pronouns = pronouns;
+            this.hobbies = new string[] { };
+        }
+        public void SetHobbies(string[] hobbies)
+            {
+            this.hobbies = hobbies;
+            }
+
+        public string ViewProfile()
+        {
+            string currentHobbies = " ";
+
+            foreach (string hobby in hobbies)
+            {
+                if (hobbies.Length > 0)
+                {
+                    currentHobbies += hobby + ", ";
+                }
+            }
+
+            string bio = $"Name : {name}, \nAge: {age}, \nCity: {city}, \nCountry: {country}, \nHobbies: {currentHobbies}";
+
+            //Console.WriteLine(bio);
+            return bio;
+          
+        }
+
+    }
+}
---
+++ TheObjectOfYourAffection/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheObjectOfYourAffection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile sam = new Profile("Sam Drakkila", 14, "New York", "USA", "he/him");
+
+            sam.SetHobbies(new string[] { "listening to audiobooks and podcasts",
+                "playing rec sports like bowling and kickball", "writing a speculative fiction novel", "reading advice colums" });
+
+            //sam.name = "Sam Drakkila";
+
+            Console.WriteLine(sam.ViewProfile());
+            //sam.ViewProfile();
+
+
+        }
+    }
+}
